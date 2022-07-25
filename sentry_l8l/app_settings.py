62554177cbbadf834fb1a8967018dbb7930ff45c
Jules Waldhart <jules.waldhart@lesoctetslibres.com>
2022-07-25 10:32:21 +0200

    feat: 🔊 Add deprecation warning about SENTRY_L8L_IGNORE_PATHS

--- sentry_l8l/app_settings.py
+++ sentry_l8l/app_settings.py
@@ -1,3 +1,15 @@
+from warnings import warn
+
 from django.conf import settings
+
+if not hasattr(settings, "SENTRY_L8L_IGNORE_PATHS"):
+    warn(
+        (
+            "The /api/is-alive/ path is not down-sampled by default anymore."
+            "Use SENTRY_L8L_IGNORE_PATHS to ignore it completely."
+        ),
+        DeprecationWarning,
+    )
+
 
 IGNORE_PATHS = getattr(settings, "SENTRY_L8L_IGNORE_PATHS", ["/api/bugs/"])
